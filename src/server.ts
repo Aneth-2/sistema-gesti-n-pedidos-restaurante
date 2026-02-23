e172d3d07c9cf11436cd8f253f28092b634c3b5f
Anet-2 <aquispe@unjbg.edu.pe>
2026-02-22 23:14:46 -0500

    Configuracion de la inyección de dependencia en el servidor

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,30 @@
+import { InMemoryMesaRepository } from "./infrastructure/repositories/MemoryMesaRepo";
+import { InMemoryPedidoRepository } from "./infrastructure/repositories/MemoryPedidoRepo";
+
 import { RegistrarMesaUseCase } from "./application/useCases/RegistrarMesa";
 import { RegistrarClienteEnMesaUseCase } from "./application/useCases/RegistrarClienteEnMesa";
 import { CrearPedidoUseCase } from "./application/useCases/CrearPedido";
 import { AgregarPlatoUseCase } from "./application/useCases/AgregarPlato";
 import { MetricasService } from "./application/services/MetricasService";
 
-const registrarMesa = new RegistrarMesaUseCase();
+// Repositorios
+const mesaRepository = new InMemoryMesaRepository();
+const pedidoRepository = new InMemoryPedidoRepository();
+
+// UseCases con inyección
+const registrarMesa = new RegistrarMesaUseCase(mesaRepository);
 const registrarCliente = new RegistrarClienteEnMesaUseCase();
-const crearPedido = new CrearPedidoUseCase();
+const crearPedido = new CrearPedidoUseCase(pedidoRepository);
 const agregarPlato = new AgregarPlatoUseCase();
 const metricas = new MetricasService();
 
-// Crear mesa
+// Flujo
 const mesa1 = registrarMesa.ejecutar(1);
-
-// Registrar cliente
 const cliente1 = registrarCliente.ejecutar(mesa1, "C1", "Carlos");
-
-// Crear pedido
 const pedido1 = crearPedido.ejecutar(cliente1, "P1");
 
-// Agregar platos
 agregarPlato.ejecutar(pedido1, "PL1", "Pizza", 35);
 agregarPlato.ejecutar(pedido1, "PL2", "Bebida", 10);
 
-// Métricas
 console.log("Clientes activos:", metricas.calcularClientesActivos(registrarMesa.obtenerMesas()));
 console.log("Total venta pedido:", pedido1.calcularTotal());
