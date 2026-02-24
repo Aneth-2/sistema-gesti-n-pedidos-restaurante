c5696c1cd1cb3811aa984033ab39959b5ee237e0
Anet-2 <aquispe@unjbg.edu.pe>
2026-02-23 23:50:04 -0500

    Implementavion de un servidor HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,27 @@
-import { InMemoryMesaRepository } from "./infrastructure/repositories/MemoryMesaRepo";
-import { InMemoryPedidoRepository } from "./infrastructure/repositories/MemoryPedidoRepo";
+import http from "http";
 
+import { InMemoryMesaRepository } from "./infrastructure/repositories/MemoryMesaRepo";
 import { RegistrarMesaUseCase } from "./application/useCases/RegistrarMesa";
-import { RegistrarClienteEnMesaUseCase } from "./application/useCases/RegistrarClienteEnMesa";
-import { CrearPedidoUseCase } from "./application/useCases/CrearPedido";
-import { AgregarPlatoUseCase } from "./application/useCases/AgregarPlato";
-import { MetricasService } from "./application/services/MetricasService";
+import { MesaController } from "./presentation/controllers/MesaController";
+import { createRouter } from "./presentation/routes/router";
 
 // Repositorios
 const mesaRepository = new InMemoryMesaRepository();
-const pedidoRepository = new InMemoryPedidoRepository();
 
-// UseCases con inyección
-const registrarMesa = new RegistrarMesaUseCase(mesaRepository);
-const registrarCliente = new RegistrarClienteEnMesaUseCase();
-const crearPedido = new CrearPedidoUseCase(pedidoRepository);
-const agregarPlato = new AgregarPlatoUseCase();
-const metricas = new MetricasService();
+// UseCases
+const registrarMesaUseCase = new RegistrarMesaUseCase(mesaRepository);
 
-// Flujo
-const mesa1 = registrarMesa.ejecutar(1);
-const cliente1 = registrarCliente.ejecutar(mesa1, "C1", "Carlos");
-const pedido1 = crearPedido.ejecutar(cliente1, "P1");
+// Controllers
+const mesaController = new MesaController(registrarMesaUseCase);
 
-agregarPlato.ejecutar(pedido1, "PL1", "Pizza", 35);
-agregarPlato.ejecutar(pedido1, "PL2", "Bebida", 10);
+// Router
+const router = createRouter(mesaController);
 
-console.log("Clientes activos:", metricas.calcularClientesActivos(registrarMesa.obtenerMesas()));
-console.log("Total venta pedido:", pedido1.calcularTotal());
+// Servidor
+const server = http.createServer(router);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`RestTrack running on http://localhost:${PORT}`);
+});
