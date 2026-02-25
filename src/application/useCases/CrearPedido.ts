cb1b215d0101c1e3eb7740c421de9446994f94cc
Aneth-2 <aquispeh@unjbg.edu.pe>
2026-02-25 15:43:38 -0500

    eliminar hack e implementar ClienteRepository en CrearPedidoUseCase

--- src/application/useCases/CrearPedido.ts
+++ src/application/useCases/CrearPedido.ts
@@ -1,14 +1,27 @@
 import { Pedido } from "../../domain/entities/Pedido";
-import { Cliente } from "../../domain/entities/Cliente";
 import { PedidoRepository } from "../../domain/repositories/PedidoRepo";
+import { ClienteRepository } from "../../domain/repositories/ClienteRepo";
 
 export class CrearPedidoUseCase {
-  constructor(private pedidoRepository: PedidoRepository) {}
 
-  public ejecutar(cliente: Cliente, pedidoId: string): Pedido {
+  constructor(
+    private pedidoRepository: PedidoRepository,
+    private clienteRepository: ClienteRepository
+  ) {}
+
+  public ejecutar(clienteId: string, pedidoId: string): Pedido {
+
+    const cliente = this.clienteRepository.findById(clienteId);
+
+    if (!cliente) {
+      throw new Error("Cliente no encontrado");
+    }
+
     const pedido = new Pedido(pedidoId, cliente.getId());
+
     cliente.asignarPedido(pedido);
     this.pedidoRepository.save(pedido);
+
     return pedido;
   }
 }
