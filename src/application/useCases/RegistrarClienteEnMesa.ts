299723ac397973f0e73dc7c664177fad3691a13a
Aneth-2 <aquispeh@unjbg.edu.pe>
2026-02-25 15:41:16 -0500

    inyectar ClienteRepository en el caso de uso RegistrarClienteEnMesa

--- src/application/useCases/RegistrarClienteEnMesa.ts
+++ src/application/useCases/RegistrarClienteEnMesa.ts
@@ -1,10 +1,17 @@
 import { Cliente } from "../../domain/entities/Cliente";
 import { Mesa } from "../../domain/entities/Mesa";
+import { ClienteRepository } from "../../domain/repositories/ClienteRepo";
 
 export class RegistrarClienteEnMesaUseCase {
+
+  constructor(private clienteRepository: ClienteRepository) {}
+
   public ejecutar(mesa: Mesa, clienteId: string, nombre: string): Cliente {
     const cliente = new Cliente(clienteId, nombre);
+
+    this.clienteRepository.save(cliente);
     mesa.agregarCliente(cliente);
+
     return cliente;
   }
 }
